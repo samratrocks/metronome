c769490d9bdb13fca4285464f743440ec593cce3
samratrocks <samrat.shiwakoti@gmail.com>
2022-03-25 18:22:56 -0400

    add slider to the gui.

--- main.rkt
+++ main.rkt
@@ -1,15 +1,10 @@
 #lang racket/gui
 
-(require rsound)
-
 ; Metronome for racket using gui
+(require rsound)
 
 
 ;; Logic section
-(define (get-tick x)
-  "Get tick at given times to call a function." x)
-
-
 (define (do-something)
   (write "something"))
 
@@ -26,25 +21,25 @@
 
 
 (define (calculate-time-for-timer bpm)
-  (* 1000 (/ bpm 60)))
+  (exact-floor (* 1000 (/ bpm 60))))
 
 
-(define (start-metronome bpm)
-  (send a-timer start (calculate-time-for-timer bpm)))
-
-
-(define (stop-metronome)
-  (send a-timer stop))
 
 
 ;; GUI Section
 (define metronome-gui-frame (new frame% [label "Main Window"]))
 
+(define tempo-slider (new slider%
+     [label "Tempo"]
+     [min-value 40]
+     [max-value 220]
+     [parent metronome-gui-frame]
+     [init-value 120]))
 
 (new button%
      [parent  metronome-gui-frame]
      [label "Start"]
-     [callback (lambda (button button-event) (start-metronome 60))])
+     [callback (lambda (button button-event) (start-metronome (parse-slider-value (send tempo-slider get-value))))])
 
 (new button%
      [parent  metronome-gui-frame]
@@ -52,3 +47,18 @@
      [callback (lambda (button button-event) (stop-metronome))])
  
 (send  metronome-gui-frame show #t)
+
+
+(define (start-metronome bpm)
+  (write (calculate-time-for-timer bpm))
+  (send a-timer start (calculate-time-for-timer bpm)))
+
+
+(define (stop-metronome)
+  (send a-timer stop))
+
+(define (parse-slider-value value)
+  "Parses the value of the slider to an integer"
+  ; For some reason slider gives back value in terms of (x * 50) / 3
+  ; so we need to multiply by 3 and divide by 50
+  (exact-floor (* 0.06 value)))
